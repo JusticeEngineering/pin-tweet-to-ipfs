0d3a2e2b1f2b8e33317cec7f9c6e717d98b4fdce
Daniel Weber <dweber@myscholly.com>
2022-09-29 17:44:51 -0300

    feat: add pin tweet button

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,40 @@
-import { printLine } from './modules/print';
+const observer = new MutationObserver(onMutation);
+observer.observe(document, {
+  childList: true,
+  subtree: true,
+});
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+function onMutation(mutations) {
+  const getTweetUrl = (e) => {
+    const selectedTweet = e.target.closest("article");
+    const tweetUrl = selectedTweet.querySelector(
+      '[data-testid="User-Names"] [role="link"][dir="auto"]'
+    )?.href;
 
-printLine("Using the 'printLine' function from the Print Module");
+    console.log("tweetUrl: ", tweetUrl);
+  };
+
+  const pinTweetButton = document.createElement("button");
+  pinTweetButton.innerHTML = "PinTweetIPFS";
+  pinTweetButton.addEventListener("click", (e) => getTweetUrl(e));
+
+  const found = [];
+  for (const { addedNodes } of mutations) {
+    for (const node of addedNodes) {
+      if (!node.tagName) continue; // not an element
+
+      if (node.firstElementChild) {
+        found.push(
+          ...node.getElementsByClassName(
+            "css-1dbjc4n r-1ta3fxp r-18u37iz r-1wtj0ep r-1s2bzr4 r-1mdbhws"
+          )
+        );
+      }
+    }
+  }
+
+  found.forEach((n) => {
+    n.style.background = "#808080";
+    n.append(pinTweetButton);
+  });
+}
