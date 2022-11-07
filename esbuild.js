5f71fd86ecfcacbaf45d1492fd9e4e2340ad1883
David Justice <work@justice.engineering>
2022-11-07 12:43:29 -0500

    Switch build system
    remove webpack, hot loader(wasn't working), babel etc..
    replace with much faster and simpler esbuild
    update dependencies

---
+++ esbuild.js
@@ -0,0 +1,42 @@
+import fs from "fs";
+import cpy from "cpy";
+import { deleteSync } from "del";
+import esbuild from "esbuild";
+
+import pkg from "./package.json" assert { type: "json" };
+import manifest from "./src/manifest.json" assert { type: "json" };
+
+deleteSync(["build/**"]);
+
+esbuild
+  .build({
+    entryPoints: [
+      "./src/background.js",
+      "./src/content.js",
+      "./src/Popup/index.jsx",
+    ],
+    bundle: true,
+    minify: true,
+    sourcemap: process.env.NODE_ENV !== "production",
+    target: ["chrome100"],
+    outdir: "./build",
+    define: {
+      "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
+    },
+  })
+  .catch(() => process.exit(1));
+
+// update manifest file version
+manifest.version = pkg.version;
+fs.writeFile(
+  "build/manifest.json",
+  JSON.stringify(manifest, null, 2),
+  (err) => {
+    if (err) throw err;
+    console.log("manifest.json updated");
+  }
+);
+
+await cpy(["src/*.png"], "build", { flat: true });
+
+await cpy(["src/Popup/popup.html"], "build/Popup/", { flat: true });
