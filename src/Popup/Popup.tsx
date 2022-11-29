ad8eb219d5f14389d72d3a08b68462d520cc85c1
David Justice <work@justice.engineering>
2022-11-28 20:53:16 -0500

    feat: Use local storage, no more api key
    - remove web3.storage api key
    - switch to https://webrecorder.github.io/save-tweet-now for
      embedding/pinning
    - Store tweets in chrome storage instead of pulling from web3.storage
    - clean up table in popup.tsx
    - add copy to clipboard for gateway url in popup

--- src/Popup/Popup.tsx
+++ src/Popup/Popup.tsx
@@ -1,85 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { Web3Storage } from "web3.storage";
+import React, { useState } from "react";
 import Card from "../components/Card/Card";
 
-type PinsType = {
-  peerId: string;
-  peerName: string;
-  region: string;
-  status: string;
-  updated: string;
-};
-
-type Web3StorageItemsType = {
-  cid: string;
-  created: string;
-  dagSize: number;
-  deals: any[];
-  name: string;
-  pins: PinsType[];
-  type?: string;
-  updated?: string;
-  _id?: string;
-};
-
 const Popup: React.FC = () => {
-  const [web3storageKey, setWeb3storageKey] = useState<string>("");
-  const [savedKey, setSavedKey] = useState<string>("");
-  const [storedItems, setStoredItems] = useState<Web3StorageItemsType[]>([]);
+  const [storedItems, setStoredItems] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isInitRun, setIsInitRun] = useState<boolean>(true);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    chrome.storage.local.set({ web3storageKey });
-    setSavedKey(web3storageKey);
+  const populateStoredItems = async () => {
+    setIsInitRun(false);
+    setIsLoading(true)
+    const result = await chrome.storage.sync.get(null)
+    setStoredItems(Object.values(result))
+    setIsLoading(false)
   };
 
-  const handleRemoveKey = () => {
-    chrome.storage.local.set({ web3storageKey: null });
-    setSavedKey("");
-    setWeb3storageKey("");
-  };
-
-  const handleWeb3Storage = async () => {
-    setIsLoading(true);
-    const client = new Web3Storage({ token: savedKey });
-    const items = [];
-    for await (const item of client.list({ maxResults: 10 })) {
-      items.push(item);
-    }
-    setStoredItems(items);
-    setIsLoading(false);
-  };
-
-  function formatKey(str: string) {
-    if (str.length > 35) {
-      return (
-        str.substr(0, 20) + "..." + str.substr(str.length - 10, str.length)
-      );
-    }
-    return str;
-  }
-
-  useEffect(() => {
-    chrome.storage.local.get(["web3storageKey"], function (result) {
-      setSavedKey(result.web3storageKey);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (savedKey) handleWeb3Storage();
-  }, [savedKey]);
+  if (isInitRun) populateStoredItems();
 
   return (
     <div className="container">
-      {savedKey ? (
+      {(
         <div>
           <div className="header">
-            <p>
-              <b>Key:</b> {formatKey(savedKey)}
-            </p>
-            <button onClick={handleRemoveKey}>Remove Key</button>
+            <h2>
+              <b>Archived Tweets</b>
+            </h2>
           </div>
           <div>
             {isLoading ? (
@@ -91,16 +35,6 @@ const Popup: React.FC = () => {
             )}
           </div>
         </div>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <label>Enter Your web3.storage Key</label>
-          <input
-            type="text"
-            value={web3storageKey}
-            onChange={(e) => setWeb3storageKey(e.target.value)}
-          />
-          <button>Save Key</button>
-        </form>
       )}
     </div>
   );
---
+++ src/copy-text-to-clipboard.js
@@ -0,0 +1,32 @@
+
+// copied from https://github.com/ipfs/ipfs-companion/pull/1078/files
+// TODO: clean up
+export default async function copyTextToClipboard(text) {
+  try {
+    try {
+      // Modern API (spotty support, but works in Firefox)
+      await navigator.clipboard.writeText(text);
+      // FUN FACT:
+      // Before this API existed we had no access to clipboard from
+      // the background page in Firefox and had to inject content script
+      // into current page to copy there:
+      // https://github.com/ipfs-shipyard/ipfs-companion/blob/b4a168880df95718e15e57dace6d5006d58e7f30/add-on/src/lib/copier.js#L10-L35
+      // :-))
+    } catch (e) {
+      // Fallback to old API (works only in Chromium)
+      // TODO replace this long type definition
+      function oncopy(event) {
+        document.removeEventListener("copy", oncopy, true);
+        event.stopImmediatePropagation();
+        event.preventDefault();
+        event.clipboardData.setData("text/plain", text);
+      }
+      document.addEventListener("copy", oncopy, true);
+      document.execCommand("copy");
+    }
+    // notify("notify_copiedTitle", text);
+  } catch (error) {
+    console.error("[pin-tweet-to-ipfs] Failed to copy text", error);
+    // notify("notify_addonIssueTitle", "Unable to copy");
+  }
+}
