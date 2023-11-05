a519849706df33447723412678ae11da976a188f
David Justice <work@justice.engineering>
2023-11-05 01:15:59 -0400

    fix error causing badge action to lose connection
    https://groups.google.com/a/chromium.org/g/chromium-extensions/c/q18Zuz0tuUs

--- src/Popup/index.jsx
+++ src/Popup/index.jsx
@@ -7,14 +7,25 @@ import Popup from './Popup'
 
 // If user is on tweet permalink page, send them to archive page and
 // close the popup
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  const currentTab = tabs[0] // there will be only one in this array
-  const url = currentTab.url.match(/https:\/\/twitter.com\/(\w){1,15}\/status\/(\d)*/)[0]
-  if (url) {
-    chrome.runtime.sendMessage({ url })
-    window.close()
-  }
-})
+function main() {
+  const root = createRoot(window.document.getElementById('app-container'))
 
-const root = createRoot(window.document.getElementById('app-container'))
-root.render(<Popup />)
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const currentTab = tabs[0] // there will be only one in this array
+      const tweetUrlMatch = currentTab.url.match(/https:\/\/twitter.com\/(\w){1,15}\/status\/(\d)*/)
+    if (tweetUrlMatch) {
+      const url = tweetUrlMatch[0]
+      root.render(<Popup />)
+      chrome.runtime.sendMessage({ url })
+      // window.close()
+    } else root.render(<Popup />)
+  })
+}
+
+
+try {
+  main()
+} catch (err) {
+  // https://groups.google.com/a/chromium.org/g/chromium-extensions/c/q18Zuz0tuUs
+  console.error('[pin-tweet-to-ipfs] Failed to add runtime listener', err)
+}
